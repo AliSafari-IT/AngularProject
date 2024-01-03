c9ceaed8e1204f2ca7e5ca6e85f34af528e9543a
asafarim <asafarim.it@gmail.com>
2024-01-03 03:03:13 +0100

    - Add search functionality to EmployeeController - Fix null reference exception in EmployeeService.AddEmployeeAsync - Refactor Employee entity model for better clarity - Update README with new API endpoints documentation - Optimize query performance in GetAllEmployeesAsync method - Add unit tests for new search API in EmployeeController

--- CrewControl.NetCoreApi/Controller/PersonsController.cs
+++ CrewControl.NetCoreApi/Controller/PersonsController.cs
@@ -2,21 +2,88 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Collections.Immutable;
+using System.Linq;
 using System.Threading.Tasks;
 
-namespace CrewControl.NetCoreApi.Controller
+namespace CrewControl.NetCoreApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PersonsController(CDbContext context) : ControllerBase
+    public class PersonsController : ControllerBase
     {
-        private readonly CDbContext _context = context;
+        private readonly CDbContext _context;
 
+        public PersonsController(CDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Persons
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Person>>> Get()
         {
             return await _context.Persons.ToListAsync();
+        }
+
+        // POST: api/Persons
+        [HttpPost]
+        public async Task<ActionResult<Person>> PostPerson(Person person)
+        {
+            _context.Persons.Add(person);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPerson", new { id = person.Id }, person);
+        }
+
+        // PUT: api/Persons/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPerson(int id, Person person)
+        {
+            if (id != person.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(person).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Persons/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePerson(int id)
+        {
+            var person = await _context.Persons.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            _context.Persons.Remove(person);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PersonExists(int id)
+        {
+            return _context.Persons.Any(e => e.Id == id);
         }
     }
 }
--- CrewControl.NetCoreApi/Model/Person.cs
+++ CrewControl.NetCoreApi/Model/Person.cs
@@ -1,8 +1,14 @@
 public class Person
 {
-    public int PersonID { get; set; }
-    public string LastName { get; set; }
+    public int Id { get; set; }
     public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
     public string Address { get; set; }
     public string City { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateTime DateOfBirth { get; set; }
 }
