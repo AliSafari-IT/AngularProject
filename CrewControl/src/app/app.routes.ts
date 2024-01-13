bbcb51c7d629794dab5629af0822dce064ad6bb7
asafarim <asafarim.it@gmail.com>
2024-01-13 16:44:15 +0100

    Page routing initialization

--- CrewControl/src/app/app.routes.ts
+++ CrewControl/src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HomePageComponent } from './pages/home/home-page/home-page.component';
+import { ChangeLogContentComponent } from './pages/change-log/change-log-content/change-log-content.component';
+import { EmployeesListComponent } from './components/employees-list/employees-list.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ChangeLog', component: ChangeLogContentComponent },
+  { path: 'Home', component: HomePageComponent},
+  { path: 'EmployeeList', component: EmployeesListComponent},
+  { path: '**', component: NotFoundComponent }
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
