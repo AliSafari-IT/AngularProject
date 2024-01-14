98e6fd0a2aa85a4a0f75e2daf5bfcd2fcea3c21a
asafarim <asafarim.it@gmail.com>
2024-01-14 04:04:24 +0100

    add routing pages as top menu for Home | Recent Changes | About | Employee List

--- CrewControl/src/app/app.component.ts
+++ CrewControl/src/app/app.component.ts
@@ -3,27 +3,35 @@ import { CommonModule } from '@angular/common';
 import { EmployeeService } from './Services/employee.service';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
-import { EmployeesListComponent } from "./components/employees-list/employees-list.component";
+import { EmployeePersonListComponent } from './components/employee-person-list/employee-person-list.component';
+import { EmployeesListComponent } from './components/employees-list/employees-list.component';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    providers: [EmployeeService],
-    imports: [
-        CommonModule,
-        ReactiveFormsModule,
-        HttpClientModule,
-        RouterOutlet,
-        EmployeesListComponent
-    ],
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  providers: [EmployeeService,],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    EmployeePersonListComponent,
+    EmployeesListComponent,
+    RouterOutlet
+  ]
 })
 
 export class AppComponent implements OnInit {
+  constructor(private employeeService: EmployeeService, private router: Router) { }
+
+  navigateTo(path: string) {
+    this.router.navigateByUrl(path);
+  }
+
   employees: any = [];
-  constructor(private employeeService: EmployeeService) { }
+
   ngOnInit(): void {
     this.employeeService.getEmployees().subscribe(data => {
       this.employees = data;
--- CrewControl/src/app/app.routes.ts
+++ CrewControl/src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HomePageComponent } from './pages/home/home-page.component';
+import { ChangeLogContentComponent } from './pages/change-log/change-log-content.component';
+import { EmployeesListComponent } from './components/employees-list/employees-list.component';
+import { AboutPageComponent } from './pages/about/about-page.component';
 
-export const routes: Routes = [];
+export const routes = [
+  { path: 'change-log', component: ChangeLogContentComponent },
+  { path: 'home', component: HomePageComponent},
+  { path: 'about', component: AboutPageComponent}, 
+  { path: 'employees-list', component: EmployeesListComponent},
+  { path: '**', component: NotFoundComponent }
+];
