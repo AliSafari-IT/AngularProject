562c92dbde1bb58423cf3f84c0cf40dc7201114d
asafarim <asafarim.it@gmail.com>
2024-01-14 03:44:16 +0100

    modified:   CrewControl/src/app/app.component.ts

--- CrewControl/src/app/app.component.ts
+++ CrewControl/src/app/app.component.ts
@@ -4,19 +4,21 @@ import { EmployeeService } from './Services/employee.service';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { EmployeesListComponent } from "./components/employees-list/employees-list.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  providers: [EmployeeService],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    RouterOutlet
-  ]
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    providers: [EmployeeService],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        RouterOutlet,
+        EmployeesListComponent
+    ]
 })
 
 export class AppComponent implements OnInit {
