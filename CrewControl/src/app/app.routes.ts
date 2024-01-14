aa14a5a6ebd4c9d956877d3bb646a0249ba79eb0
asafarim <asafarim.it@gmail.com>
2024-01-14 04:08:03 +0100

    modified:   CrewControl/src/app/app.routes.ts

--- CrewControl/src/app/app.routes.ts
+++ CrewControl/src/app/app.routes.ts
@@ -1,8 +1,9 @@
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { HomePageComponent } from './pages/home/home-page.component';
-import { ChangeLogContentComponent } from './pages/change-log/change-log-content.component';
-import { EmployeesListComponent } from './components/employees-list/employees-list.component';
-import { AboutPageComponent } from './pages/about/about-page.component';
+import { EmployeesListComponent } from "./components/employees-list/employees-list.component";
+import { AboutPageComponent } from "./pages/about/about-page.component";
+import { ChangeLogContentComponent } from "./pages/change-log/change-log-content.component";
+import { HomePageComponent } from "./pages/home/home-page.component";
+import { NotFoundComponent } from "./pages/not-found/not-found.component";
+
 
 export const routes = [
   { path: 'change-log', component: ChangeLogContentComponent },
